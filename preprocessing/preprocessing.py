fe3fdd56dfb71bfa488abb490cae81cfe05b2327
siebert <looijesiebert@gmail.com>
2016-05-02 22:54:04 +0200

    added python files

---
+++ preprocessing/preprocessing.py
@@ -0,0 +1,9 @@
+import pycpsp.sourceindicators.config as config
+import pycpsp.sourceindicators.util as util
+import pycpsp.files as files
+import pycpsp.plot as plot
+import os, matplotlib.pyplot as plt
+import logging
+logger = logging.getLogger('Jupyter')
+logger.setLevel(logging.INFO)
+
